f74b67fdb3280d243fabc1f4ec5e33a415a60bf9
Theo Long <theodorelong98@gmail.com>
2026-03-24 13:19:18 -0400

    cust 0 sits at 0

--- kc/observation_weights.py
+++ kc/observation_weights.py
@@ -320,6 +320,9 @@ class DirichletProcessWeight(WeightType):
     def get_log_likelihood(self, **kwargs) -> tuple[float | None, int]:
         log_likelihood = 0.0
         for process, cluster_assignment in self.cluster_assignments.items():
+            # customer 0 always sits at table 0, this is implict in the representation
+            # TODO - this is hacky and mutates the SPN when you call get_log_likelihood!
+            cluster_assignment[0] = 0
             alpha = kwargs["dp_priors"][process]
             N = len(cluster_assignment)
             table_sizes = Counter(cluster_assignment.values())
