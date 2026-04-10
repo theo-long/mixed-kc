dde0aa9a9aeedc41a2b8749524668de0909e0cdd
Theo Long <theodorelong98@gmail.com>
2026-04-10 18:08:57 -0400

    wip

--- kc/dirichlet_process.py
+++ kc/dirichlet_process.py
@@ -17,8 +17,15 @@ class DirichletPartitionWeight(WeightType):
     def scope(self):
         return set(self.partitions.keys())
 
-    def __mul__(self: Self, other) -> Self:
-        raise NotImplementedError()
+    def __mul__(self: Self, other: Self) -> "DirichletPartitionWeight | None":
+        partitions = self.partitions
+        for key, partition in other.partitions.items():
+            if key in partitions:
+                partition = partitions[key] * partition
+            if partition is None:
+                return
+            partitions[key] = partition
+        return DirichletPartitionWeight(partitions)
 
     def get_log_likelihood(self, **kwargs) -> tuple[float | None, int]:
         raise NotImplementedError()
